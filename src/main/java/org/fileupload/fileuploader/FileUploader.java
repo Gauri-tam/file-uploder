9cd6e6d95ff0f4bb045e23dbfb20b5f13ccc3f76
gauri <gauri@weetechsolution.com>
2025-05-15 17:32:28 +0530

    Updating the File Uploader

--- src/main/java/org/fileupload/fileuploader/FileUploader.java
+++ src/main/java/org/fileupload/fileuploader/FileUploader.java
@@ -12,33 +12,6 @@ public class FileUploader extends Application {
 
     Logger logger = LoggerConfig.logger;
 
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//        logger.info("Application Start...");
-//        FXMLLoader fxmlLoader = new FXMLLoader(FileUploader.class.getResource("file-uploader.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 1050, 1000);
-//
-//        // Step 1: Hardcoded AWS credentials  // we can create a configuration for this
-//        String accessKey = "YOUR_ACCESS_KEY";
-//        String secretKey = "YOUR_SECRET_KEY";
-//        String bucketName = "your-bucket-name";
-//        String region = "us-east-1";
-//
-//        // Step 2: Initialize S3Service
-//        S3Service s3Service = new S3Service(accessKey, secretKey, bucketName, region);
-//
-//        // Step 3: Inject S3Service into the controller
-//        FileUploaderController controller = fxmlLoader.getController();
-//        controller.setPrimaryStage(primaryStage);
-//        controller.setS3Service(s3Service);
-//
-//        // Step 4: Setup stage
-//        primaryStage.setTitle("File Upload Example");
-//        primaryStage.setResizable(true);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         logger.info("Application Start...");
